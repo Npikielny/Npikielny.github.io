c327010c89338710b33ebb3b86be83c69a025f28
Noah Pikielny <npikielny@gmail.com>
2026-06-01 15:58:37 -0400

    Updating for Chirayath lab

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const splash = document.querySelector('.splash');
+// const splash = document.querySelector('.splash');
 
-document.addEventListener('DOMContentLoaded', (e) => {
-  setTimeout(() => {
-    window.scrollTo(0, 0);
-    splash.classList.add('hidden');
-  }, 1500)
-})
+// document.addEventListener('DOMContentLoaded', (e) => {
+//   setTimeout(() => {
+//     window.scrollTo(0, 0);
+//     splash.classList.add('hidden');
+//   }, 1500)
+// })
